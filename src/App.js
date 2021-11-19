5695126374fed50fd676b83cc11ffc35d14dfe2f
Luis Alberto <92551372+Betomx45@users.noreply.github.com>
2021-11-19 08:33:10 -0600

    se ha modificado el proyecto

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React hola este es mi proyecto en git
+          Learn React 
         </a>
       </header>
     </div>
