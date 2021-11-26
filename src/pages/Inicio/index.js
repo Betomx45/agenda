4f3e64ef43124f8083f89f9ed9a1fc05eff4907b
Luis Alberto <92551372+Betomx45@users.noreply.github.com>
2021-11-26 10:39:23 -0600

    actualizado

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import React, { useEffect, useState } from "react";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import DeleteIcon from '@mui/icons-material/Delete';
